1675339b84fc541da8171147dadb683c10ed12c4
Francesco Faraone <ffaraone@gmail.com>
2018-07-06 20:56:45 +0200

    wip getting images

--- mopidy_moppina/handlers.py
+++ mopidy_moppina/handlers.py
@@ -3,10 +3,40 @@ from __future__ import unicode_literals
 import json
 import logging
 from pprint import pformat
+import requests
 
 import tornado.web
 
 logger = logging.getLogger(__name__)
+
+SIZE_TABLE = {
+    '': {
+        'width': 300,
+        'height': 300
+    },
+    'small': {
+        'width': 300,
+        'height': 300
+    },
+    'medium': {
+        'width': 300,
+        'height': 300
+    },
+    'large': {
+        'width': 300,
+        'height': 300
+    },
+    'extralarge': {
+        'width': 300,
+        'height': 300
+    },
+    'mega': {
+        'width': 300,
+        'height': 300
+    }
+}
+
+
 
 class HttpHandler(tornado.web.RequestHandler):
 
@@ -17,6 +47,9 @@ class HttpHandler(tornado.web.RequestHandler):
     def initialize(self, core, config):
         self.core = core
         self.config = config
+
+        self.lastfm_key = 'd766f7c820cd47404b325ab3e5c4fe0a'
+        self.lastfm_endpoint = 'http://ws.audioscrobbler.com/2.0/'
 
     # Options request
     # This is a preflight request for CORS requests
@@ -41,6 +74,33 @@ class HttpHandler(tornado.web.RequestHandler):
                 to_query.append(uri)
         return results, to_query
 
+    def get_art(self, artist, album=None):
+        ret = []
+        payload = {
+            'api_key': self.lastfm_key,
+            'method': 'artist.getinfo' if not album else 'album.getinfo',
+            'format': 'json',
+            'artist': artist
+        }
+        if album:
+            payload['album'] = album
+        
+
+        res = requests.get(self.lastfm_endpoint, params=payload)
+
+        if res.status_code == 200:
+            data = res.json()
+            key = 'artist' if not album else 'album'
+            for img in data.get(key, {}).get('image', []):
+                try:
+                    img_data = dict(uri=img['#text'])
+                    img_data.update(SIZE_TABLE[img['size']])
+                    ret.append(img_data)
+                except:
+                    pass
+        return ret
+
+
     @tornado.web.asynchronous
     def post(self, *args, **kwargs):
 
@@ -57,11 +117,11 @@ class HttpHandler(tornado.web.RequestHandler):
         
         logger.info('Moppina: search for images')
 
-        req = json.loads(self.request.body.decode('utf-8'))
-        uris = req.get('uris', [])
+        uris = json.loads(self.request.body.decode('utf-8'))
         
         # remove toptrack sauce from spotifyweb uris
-        uris = filter(lambda x: 'spotifyweb:sauce:artist-toptracks' not in x, uris)
+        toptrack_uri = 'spotifyweb:sauce:artist-toptracks'
+        uris = filter(lambda x: toptrack_uri not in x, uris)
         # remove other kind of sauce from spotifyweb uris
         uris = map(fix_spotifyweb_uris, uris)
 
@@ -75,12 +135,21 @@ class HttpHandler(tornado.web.RequestHandler):
             results.update(r)
             to_query.extend(q)
         if len(to_query) > 0:
-            for uri in to_query:
-                lookup_results = self.core.library.lookup(uri=uri).get()
-                # logger.info('*' * 50)
-                # logger.info('lookup results %s', lookup_results) 
-                # # for uri, tracks in lookup_results.iteritems():
-                # #     logger.info('%s %s', uri, pformat(tracks))
-                # logger.info('*' * 50)
+            lookup_results = self.core.library.lookup(uris=to_query).get()
+            for uri, tracks in lookup_results.iteritems():
+                logger.info('found %s result for uri %s', len(tracks), 
+                            uri)
+                if len(tracks) > 0:
+                    track = tracks[0]
+                    if uri.startswith('local:artist'):
+                        artist = next(iter(track.artists)).name
+                        results[uri] = self.get_art(artist)
+                    elif uri.startswith('local:album') or \
+                            uri.startswith('local:track'):
+                        artist = next(iter(track.artists)).name
+                        album = track.album.name
+                        results[uri] = self.get_art(artist, album)
+                    else:
+                        logger.warning('unsupported uri %s', uri)
         self.write(results)
         self.finish()
